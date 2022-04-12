01ede553d873b18582be246e60de4c60d3bff348
MarkuSchick <markuschick@gmail.com>
2022-04-12 15:09:40 +0200

    feat: add motivation

--- src/pages/Motivation.jsx
+++ src/pages/Motivation.jsx
@@ -2,8 +2,22 @@ export default function Motivation(props) {
   // html representation
   return (
     <div className="Motivation">
-      <h1> Why are you doing this? Some additional words</h1>
-      <h1> Why are you doing this? Some additional words</h1>
+      <h1> Should your stake your ETH or not?</h1>
+      <h1>
+        It is frequently recommended to stake all of your ETH to generate yield
+      </h1>
+
+      <h1>
+        However that ignores tax effects of generating yield on your assets.
+      </h1>
+      <h1>
+        In German tax code that can result in you having to pay income tax on
+        all gains.
+      </h1>
+      <h1>
+        This app aims to answer the question: Should you stake you ETH (with
+        German tax code) or not?
+      </h1>
     </div>
   );
 }
